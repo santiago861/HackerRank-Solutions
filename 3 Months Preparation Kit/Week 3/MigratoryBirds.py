d52cc79f2ad6a65ec50eaa89b9607f55c69eac9a
santiago861 <santiago_reyes@ciencias.unam.mx>
2025-01-18 20:19:46 -0600

    repo initialized

---
+++ 3 Months Preparation Kit/Week 3/MigratoryBirds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):    
+    birds = {}
+    for elem in arr:
+        if not elem in birds:
+            birds[elem] = 1
+        else:
+            birds[elem] += 1
+    return max(birds.values())
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
